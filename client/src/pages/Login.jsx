d6939cd5cba70a0b57c1a69a2928e6d41b9467ff
Mikael <luanmikaelmartins@gmail.com>
2024-10-02 14:15:52 -0300

    Login (Front) Page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../styles/Login.css'
+
+import user_icon from '../assets/person.png'
+import password_icon from '../assets/password.png'
+
+export const Login = () => {
+  return (
+    <div className = 'container'>
+        <div className="header">
+            <div className="text">Login</div>
+            <div className="underline"></div>
+        </div>
+        <div className="inputs">
+            <div className="input">
+                <img src={user_icon} alt="" />
+                <input type="text" placeholder='Username' />
+            </div>
+            <div className="input">
+                <img src={password_icon} alt="" />
+                <input type="password" placeholder='Password' />
+            </div>
+        </div>
+        <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
+        <div className="submit-container">
+            <div className="submit">Submit</div>
+        </div>
+    </div>
+  )
+}
