1599d1eef80a236b5be2da569dcbc65557d0f9c2
Mikael <luanmikaelmartins@gmail.com>
2024-10-03 16:23:58 -0300

    Update Styles (Login & Home Page)

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,24 +6,26 @@ import password_icon from '../assets/password.png'
 
 export const Login = () => {
   return (
-    <div className = 'container'>
-        <div className="header">
-            <div className="text">Login | Admin</div>
-            <div className="underline"></div>
-        </div>
-        <div className="inputs">
-            <div className="input">
-                <img src={user_icon} alt="" />
-                <input type="text" placeholder='Username' />
+    <div className="primary-login">
+        <div className = 'container'>
+            <div className="header">
+                <div className="text">Login | Admin</div>
+                <div className="underline"></div>
             </div>
-            <div className="input">
-                <img src={password_icon} alt="" />
-                <input type="password" placeholder='Password' />
+            <div className="inputs">
+                <div className="input">
+                    <img src={user_icon} alt="" />
+                    <input type="text" placeholder='Username' />
+                </div>
+                <div className="input">
+                    <img src={password_icon} alt="" />
+                    <input type="password" placeholder='Password' />
+                </div>
             </div>
-        </div>
-        <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
-        <div className="submit-container">
-            <div className="submit">Submit</div>
+            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
+            <div className="submit-container">
+                <div className="submit">Submit</div>
+            </div>
         </div>
     </div>
   )
