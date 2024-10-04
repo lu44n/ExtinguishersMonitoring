43cc08bb5d42719b7117aa7c2fcab7fd0114005b
Mikael <luanmikaelmartins@gmail.com>
2024-10-03 22:12:03 -0300

    Some changes

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/" element={<Login />} />
         <Route 
-          path="/" 
+          path="/home" 
           element={
             <>
               <Navbar />
