e0e2a190ac9298da35df1ec2e5ada992c40b555e
Mikael <luanmikaelmartins@gmail.com>
2024-10-09 23:08:20 -0300

    Página de Consulta (Front-End) - Pronta

--- client/src/pages/Consulta.jsx
+++ client/src/pages/Consulta.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Consulta.css'
+import { FireExtinguisherCard } from '../components/FireExtinguisherCard';
 
 export const Consulta = () => {
   return (
-    <div>Consulta</div>
+    <div className="primary-consulta">
+        <div className="consultar-title-container">
+            <div className="title-consulta">Consulta de Extintores | Metrô SP</div>
+        </div>
+        <div className="div-search-extinguisher">
+            <input type="text" className="search-extinguisher" placeholder="Insira o ID do Extintor"/> 
+            <div className="search-bttn"><FontAwesomeIcon icon={faMagnifyingGlass} /> </div>
+        </div>
+        <div className="center-card">
+            <FireExtinguisherCard />
+        </div>
+    </div>
   )
 }
