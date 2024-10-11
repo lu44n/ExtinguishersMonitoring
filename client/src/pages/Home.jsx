c5cc50cb025db614c58f222d7a10c839b817ec57
Mikael <luanmikaelmartins@gmail.com>
2024-10-11 10:28:00 -0300

    Agora os valores fictícios da Home foram alterados para os da API

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFireExtinguisher } from '@fortawesome/free-solid-svg-icons';
 import { Navbar } from '../components/Navbar';
 import { Footer } from '../components/Footer';
-import '../styles/Home.css'
+import '../styles/Home.css';
 
 export const Home = () => {
+  const [extinguisherData, setExtinguisherData] = useState({
+    total: 0,
+    nearMaintenance: 0,
+    alertStatus: 0,
+  });
+
+  const fetchExtinguisherData = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/api/extinguishers');
+      if (!response.ok) {
+        throw new Error('Erro ao buscar dados dos extintores');
+      }
+      const data = await response.json();
+      setExtinguisherData({
+        total: data.total,
+        nearMaintenance: data.nearMaintenance,
+        alertStatus: data.alertStatus,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchExtinguisherData();
+  }, []);
+
   return (
     <div>
       <Navbar />
@@ -19,23 +46,23 @@ export const Home = () => {
             Este projeto tem como objetivo centralizar e simplificar o controle de informações sobre extintores de incêndio localizados nas estações do metrô. Com uma interface intuitiva, os usuários poderão consultar, adicionar, editar e remover registros de extintores, garantindo a segurança e a conformidade com as normas de segurança. Junte-se a nós na missão de manter o Metrô de São Paulo seguro e preparado para qualquer eventualidade!
           </div>
           <div className="title-boxes">Informações Importantes</div>
-          <div class="boxs-container">
-            <div class="info-box">
+          <div className="boxs-container">
+            <div className="info-box">
               <div className="info-text">Total de Extintores: </div>
-                <div className="extintor-number">1259</div>
+              <div className="extintor-number">{extinguisherData.total}</div>
             </div>
-            <div class="info-box">
+            <div className="info-box">
               <div className="info-text">Próximos da Inspeção: </div>
-                <div className="extintor-inspecao">128</div>
+              <div className="extintor-inspecao">{extinguisherData.nearMaintenance}</div>
             </div>
-            <div class="info-box">
+            <div className="info-box">
               <div className="info-text">Extintores em Alerta: </div>
-                <div className="extintor-alerta">19</div>
+              <div className="extintor-alerta">{extinguisherData.alertStatus}</div>
             </div>
           </div>
         </div>
       </div>
       <Footer />
     </div>
-  )
+  );
 }
