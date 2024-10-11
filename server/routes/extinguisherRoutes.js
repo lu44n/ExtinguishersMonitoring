deb16b53f588ece6a6675c65a50d82a5ac348f30
Mikael <luanmikaelmartins@gmail.com>
2024-10-11 09:47:07 -0300

    Novo router de Editar Informações dos Extintores

--- server/routes/extinguisherRoutes.js
+++ server/routes/extinguisherRoutes.js
@@ -29,5 +29,28 @@ router.get("/extinguisher/:id", async (req, res) => {
       res.status(500).json({ message: "Erro no servidor" });
     }
   });
+
+  router.put("/extinguisher/:id", async (req, res) => {
+    const { id } = req.params;
+    const { tipo, capacidade, codigo_fabricante, data_fabricacao, data_validade, ultima_recarga, proxima_inspecao, status, localizacao } = req.body;
+  
+    try {
+      const result = await pool.query(
+        `UPDATE extintores 
+        SET tipo = $1, capacidade = $2, codigo_fabricante = $3, data_fabricacao = $4, data_validade = $5, ultima_recarga = $6, proxima_inspecao = $7, status = $8, localizacao = $9 
+        WHERE id = $10 RETURNING *`,
+        [tipo, capacidade, codigo_fabricante, data_fabricacao, data_validade, ultima_recarga, proxima_inspecao, status, localizacao, id]
+      );
+  
+      if (result.rows.length === 0) {
+        return res.status(404).json({ message: "Extintor não encontrado" });
+      }
+  
+      res.json(result.rows[0]);
+    } catch (error) {
+      console.error("Erro ao atualizar extintor:", error.message);
+      res.status(500).json({ message: "Erro no servidor" });
+    }
+  });
   
   module.exports = router;
